cc881bc5887a0de131477888e45d0abb262149ad
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2021-02-07 14:04:47 -0800

    test rabbit name

--- src/Rabbit_Driver.java
+++ src/Rabbit_Driver.java
@@ -1,4 +1,14 @@
-
+import java.util.Scanner;
 public class Rabbit_Driver
 {
+    public static void main(String[] args)
+    {
+        Rabbit_RegEx rabbit = new Rabbit_RegEx();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a rabbit name:");
+        rabbit.setRabbitName(input.nextLine());
+        System.out.println("Rabbit's Name is: " + rabbit.getRabbitName());
+        
+    }
 }
