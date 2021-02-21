b5617e8f425bf09c44d816f1276f46e5e2e7cd64
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2021-02-21 15:59:15 -0800

    commented out driver to not run into problems while doing tests

--- src/Rabbit_Driver.java
+++ src/Rabbit_Driver.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 public class Rabbit_Driver
 {
-    public static void main(String[] args)
-    {
-        Rabbit_RegEx rabbit = new Rabbit_RegEx();
-
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a rabbit name:");
-        rabbit.setRabbitName(input.nextLine());
-        System.out.println("Rabbit's Name is: " + rabbit.getRabbitName());
-
-        System.out.println("Enter a breed name:");
-        rabbit.setBreedName(input.nextLine());
-        System.out.println("Breed Name is: " + rabbit.getBreedName());
-
-        System.out.println("Enter true or false for Baby Bunny: ");
-        rabbit.setIsBaby(input.nextBoolean());
-        System.out.println("Is it a Baby Bun? " + rabbit.getIsBaby());
-
-        System.out.println("Enter age:");
-        rabbit.setAge(input.nextLine());
-        System.out.println("Rabbit age: " + rabbit.getAge());
-
-        System.out.println("Enter weight");
-        rabbit.setWeight(input.nextLine());
-        System.out.println("Rabbit's weight: " + rabbit.getWeight());
-    }
+//    public static void main(String[] args)
+//    {
+//        Rabbit_RegEx rabbit = new Rabbit_RegEx();
+//
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter a rabbit name:");
+//        rabbit.setRabbitName(input.nextLine());
+//        System.out.println("Rabbit's Name is: " + rabbit.getRabbitName());
+//
+//        System.out.println("Enter a breed name:");
+//        rabbit.setBreedName(input.nextLine());
+//        System.out.println("Breed Name is: " + rabbit.getBreedName());
+//
+//        System.out.println("Enter true or false for Baby Bunny: ");
+//        rabbit.setIsBaby(input.nextBoolean());
+//        System.out.println("Is it a Baby Bun? " + rabbit.getIsBaby());
+//
+//        System.out.println("Enter age:");
+//        rabbit.setAge(input.nextLine());
+//        System.out.println("Rabbit age: " + rabbit.getAge());
+//
+//        System.out.println("Enter weight");
+//        rabbit.setWeight(input.nextLine());
+//        System.out.println("Rabbit's weight: " + rabbit.getWeight());
+//    }
 }
