e1f39824aab85ca755a065f7bb785cab81b03b19
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2021-02-22 17:03:00 -0800

    test lowercase agouti color

--- RabbitsTest/Rabbit_RegExTest.java
+++ RabbitsTest/Rabbit_RegExTest.java
@@ -228,4 +228,19 @@ class Rabbit_RegExTest
                 "Color: Agouti";
         assertEquals(expected, rabbit_color.toString());
     }//end check Agouti color
+
+    @Test
+    void testLowercaseAgoutiColor()
+    {
+        Rabbit_RegEx rabbit_color = new Rabbit_RegEx();
+        rabbit_color.setIsBaby(true);
+        rabbit_color.setWeight(2);
+        rabbit_color.setAge(4);
+        rabbit_color.setColor("agouti");
+        String expected = "Is the rabbit a baby?: true\n" +
+                "How old is the rabbit?: 4 weeks\n" +
+                "Weight: 2.0 in ounces\n" +
+                "Color: agouti";
+        assertEquals(expected, rabbit_color.toString());
+    }//end lowercase agouti
 }//end class test don't try to put tests underneath this!!
