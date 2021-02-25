0aa6ba3d0f0b752370d9d24e13db62a1abbce249
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2021-02-24 16:22:48 -0800

    tested uppercase color lilac tortoiseshell

--- RabbitsTest/Rabbit_RegExTest.java
+++ RabbitsTest/Rabbit_RegExTest.java
@@ -1303,6 +1303,19 @@ class Rabbit_RegExTest
         assertEquals(expected, rabbit_color.toString());
     }//end lowercase color lilac steel
 
+    @Test //TEST FIFTY-FIVE
+    void testUppercaseLilacTortoiseshellColor()
+    {
+        Rabbit_RegEx rabbit_color = new Rabbit_RegEx();
+        rabbit_color.setColor("Lilac Tortoiseshell");
+        String expected = "Is the rabbit a baby?: false\n" +
+                "How old is the rabbit?: 0 years\n" +
+                "Weight: 0.0 in pounds\n" +
+                "Color: Lilac Tortoiseshell\n" +
+                "Gender: male";
+        assertEquals(expected, rabbit_color.toString());
+    }//end uppercase color lilac tortoiseshell
+
 
     //Test True Dwarf Gene!
     @Test //TEST ONE
