1e50eda796917af4be7ac58e3178d6bbcd695121
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2021-02-24 16:18:09 -0800

    tested lowercase breed name cavies

--- RabbitsTest/Rabbit_RegExTest.java
+++ RabbitsTest/Rabbit_RegExTest.java
@@ -284,6 +284,15 @@ class Rabbit_RegExTest
         assertEquals(expected, rabbit_breed.getBreedName());
     }//end validate breed name Cavies
 
+    @Test //TEST TWENTY-EIGHT
+    void testLowercaseCaviesBreedname()
+    {
+        Rabbit_RegEx rabbit_breed = new Rabbit_RegEx();
+        rabbit_breed.setBreedName("cavies");
+        String expected = "cavies";
+        assertEquals(expected, rabbit_breed.getBreedName());
+    }//end validate breed name cavies
+
 
     //Testing ages and if it's a baby bun!!
     @Test //TEST ONE
