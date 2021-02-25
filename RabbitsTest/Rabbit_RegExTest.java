19489906e541ada8e4aaef7bb03b5eec963d66d6
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2021-02-24 20:25:07 -0800

    tested uppercase color sandy

--- RabbitsTest/Rabbit_RegExTest.java
+++ RabbitsTest/Rabbit_RegExTest.java
@@ -1762,6 +1762,19 @@ class Rabbit_RegExTest
         assertEquals(expected, rabbit_color.toString());
     }//end lowercase color sable point
 
+    @Test //TEST SEVENTY-THREE
+    void testUppercaseSandyColor()
+    {
+        Rabbit_RegEx rabbit_color = new Rabbit_RegEx();
+        rabbit_color.setColor("Sandy");
+        String expected = "Is the rabbit a baby?: false\n" +
+                "How old is the rabbit?: 0 years\n" +
+                "Weight: 0.0 in pounds\n" +
+                "Color: Sandy\n" +
+                "Gender: male";
+        assertEquals(expected, rabbit_color.toString());
+    }//end uppercase color sandy
+
 
     //Test True Dwarf Gene!
     @Test //TEST ONE
