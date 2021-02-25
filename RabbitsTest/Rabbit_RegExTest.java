b287ded1249743ef052ec500a36ec5ca035a2c77
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2021-02-24 16:13:26 -0800

    tested uppercase color lilac steel

--- RabbitsTest/Rabbit_RegExTest.java
+++ RabbitsTest/Rabbit_RegExTest.java
@@ -1244,6 +1244,22 @@ class Rabbit_RegExTest
         assertEquals(expected, rabbit_color.toString());
     }//end lowercase color lilac chinchilla
 
+    @Test //TEST FIFTY-THREE
+    void testUppercaseLilacSteelColor()
+    {
+        Rabbit_RegEx rabbit_color = new Rabbit_RegEx();
+        rabbit_color.setIsBaby(true);
+        rabbit_color.setWeight(2);
+        rabbit_color.setAge(4);
+        rabbit_color.setColor("Lilac Steel");
+        String expected = "Is the rabbit a baby?: true\n" +
+                "How old is the rabbit?: 4 weeks\n" +
+                "Weight: 2.0 in ounces\n" +
+                "Color: Lilac Steel\n" +
+                "Gender: male";
+        assertEquals(expected, rabbit_color.toString());
+    }//end uppercase color lilac steel
+
 
     //Test True Dwarf Gene!
     @Test //TEST ONE
